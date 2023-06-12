478eb5fa60fee58e1db9c12f4671a824622c708b
LukasJandl <jandl.lukas@gmail.com>
2023-06-12 18:48:29 +0200

    refactor: change generation type of book id from standard to identity

--- backend/src/main/java/com/codecool/booktracker/model/Book.java
+++ backend/src/main/java/com/codecool/booktracker/model/Book.java
@@ -1,15 +1,11 @@
 package com.codecool.booktracker.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.aspectj.lang.annotation.RequiredTypes;
 
 @Getter
 @Setter
@@ -20,7 +16,7 @@ import org.aspectj.lang.annotation.RequiredTypes;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Book {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String author;
