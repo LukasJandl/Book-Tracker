6f613c71ea238b34f87495348c1866d1f7404d08
LukasJandl <jandl.lukas@gmail.com>
2023-06-19 18:51:18 +0200

    feat: add new Bookshelf page and Navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./components/pages/Home";
+import Navbar from "./components/Navbar";
+import Bookshelf from "./components/pages/Bookshelf";
 
 export default function App() {
     return (
-        <Routes>
-            <Route path="/" exact element={<Home />}></Route>
-        </Routes>
+        <>
+            <Navbar />
+            <Routes>
+                <Route path={"/"} exact element={<Home />} />
+                <Route path={"/Bookshelf"} element={<Bookshelf />} />
+            </Routes>
+        </>
     );
 }
